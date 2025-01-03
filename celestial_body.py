2837374bc446660cd376ebf0e8a9efeff11f1756
Vilgot Lundborg <vl222kz@student.lnu.se>
2025-01-03 17:32:04 +0100

    add color variation

--- celestial_body.py
+++ celestial_body.py
@@ -1,9 +1,23 @@
 from scene_object import SceneObject
 from sphere import generateSphere
+from math import sin, pi
 
 class CelestialBody(SceneObject):
 
     def __init__(self, program_reference, model_matrix_uniform, position_variable_name, color_variable_name, base_color):
         positions = generateSphere()
-        colors = base_color * len(positions)
+        colors = self.generate_colors(positions, base_color)
         super().__init__(program_reference, model_matrix_uniform, position_variable_name, positions, color_variable_name, colors)
+
+
+    def generate_colors(self, positions, base_color):
+        colors = []
+        for position in positions:
+            color = []
+            for i in range(3):
+                variation = sin(position[i] * pi * 4) * 0.2
+                channel_value = base_color[i] + variation
+                channel_value = max(min(channel_value, 1.0), 0.0)
+                color.append(channel_value)
+            colors.append(color)
+        return colors
--- solar_system.py
+++ solar_system.py
@@ -23,7 +23,7 @@ class SolarSystem(Program):
         self.sun = CelestialBody(self.program, self.MODEL_MATRIX_UNIFORM, self.POSITION_VARIABLE, self.COLOR_VARIABLE, [1.0, 1.0, 0.0])
         self.sun.scale(3)
 
-        self.earth = CelestialBody(self.program, self.MODEL_MATRIX_UNIFORM, self.POSITION_VARIABLE, self.COLOR_VARIABLE, [0.0, 0.0, 1.0])
+        self.earth = CelestialBody(self.program, self.MODEL_MATRIX_UNIFORM, self.POSITION_VARIABLE, self.COLOR_VARIABLE, [0.2, 0.8, 0.5])
         self.earth.translate(-6, 0, 0)
 
         camera = Camera(self.program, self.PROJECTION_VIEW_MATRIX_UNIFORM)
@@ -38,6 +38,7 @@ class SolarSystem(Program):
     def update_scene(self, dt, time):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
+        self.sun.rotate_around_y(-dt)
         self.sun.render()
 
         self.earth.rotate_around_y(dt)
